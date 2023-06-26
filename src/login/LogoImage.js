bb3ba585a3e851daba0bde05da7c06ab45d99323
Hafil Latheef <iolathief108@gmail.com>
2023-06-26 10:42:50 +0530

    init

---
+++ src/login/LogoImage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import { ReactComponent as Logo } from '../resources/images/logo.svg';
+
+const useStyles = makeStyles(() => ({
+  image: {
+    alignSelf: 'center',
+    maxWidth: '240px',
+    maxHeight: '120px',
+    width: 'auto',
+    height: 'auto',
+  },
+}));
+
+const LogoImage = ({ color }) => {
+  const classes = useStyles();
+
+  return (
+    // <Logo className={classes.image} style={{ color }} 
+    <img src="/logo192.png"
+  />);
+};
+
+export default LogoImage;
